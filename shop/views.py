9082b5a9b4e9af2adfbd6b6aa22d768653c7e30e
Washington-Ksea <ksea.washington@gmail.com>
2019-08-11 23:22:51 +0900

    customアカウントの作成

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+from config.settings import STRIPE_PUBLISHABLE_KEY, STRIPE_SECRET_KEY
+
+import stripe
+
+stripe.api_key = STRIPE_SECRET_KEY
+
+def create_custom_account():
+    account = stripe.Account.create(
+        type="custom",
+        country="JP",
+        email="test@test.com"
+    )
+    return account
+
 class ShopView(TemplateView):
     template_name='home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ShopView, self).get_context_data(**kwargs)
+        
+        print("home context!!!")
+        account = create_custom_account()
+        """
+        ...
+        "id": "acct_1F6HqzJqJdl62Rgc",        
+        ...
+        "type": "custom"
+        """
+        return context
+    
